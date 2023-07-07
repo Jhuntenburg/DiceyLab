7b0cb5c9306d7e589a0cdebadc78281545940711
Joe <Jhuntenburg@gmail.com>
2023-07-07 18:59:54 -0400

    making final changes

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -25,7 +25,8 @@ public class Simulation {
     public void printResults(){
         System.out.println("Simulation of " + numOfDice+ " dice tossed for "+ numOfRolls +" times.");
         for (int i = numOfDice; i <= (6* numOfDice); i++){
-            System.out.println(i + " : " + runBins.getBin(i) + "           :" + ((double)(runBins.getBin(i))/ numOfRolls));
+//            System.out.println(i + " : " + runBins.getBin(i) + "           :" + ((double)(runBins.getBin(i))/ numOfRolls));
+            System.out.printf("%d : %,d           :%.2f\n", i ,runBins.getBin(i), ((double)(runBins.getBin(i))/ numOfRolls));
         }
 
 
